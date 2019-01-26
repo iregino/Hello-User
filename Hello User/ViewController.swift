3ba15bfaf991f1b9030c549c0307505f93284e00
student14 <student14@student14s-macbook-air.local>
2019-01-25 22:49:07 -0600

    Initial Commit

---
+++ Hello User/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello User
+//
+//  Created by student14 on 1/25/19.
+//  Copyright © 2019 student14. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
